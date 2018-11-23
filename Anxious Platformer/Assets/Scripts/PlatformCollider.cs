f197d4d6c2c34eaaf6c0474cbe8caca8bfaaedfb
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-11-23 16:48:34 +0800

    I wanna die

--- Anxious Platformer/Assets/Scripts/PlatformCollider.cs
+++ Anxious Platformer/Assets/Scripts/PlatformCollider.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class PlatformCollider : MonoBehaviour {
     GameObject player;
     CapsuleCollider2D playerCol;
+    Rigidbody2D playerRB;
     BoxCollider2D myBCol;
 
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         playerCol = player.GetComponent<CapsuleCollider2D>();
+        playerRB = player.GetComponent<Rigidbody2D>();
         myBCol = GetComponent<BoxCollider2D>();
 	}
 	
-	void FixedUpdate () {
-        //transform.position.y - 0.5 * playerCol.size.y
-		if (player.transform.position.y - 0.25f * playerCol.size.y > transform.position.y + 0.25f * myBCol.size.y) {
-            myBCol.enabled = true;
+	void Update () {
+        if (playerRB.velocity.y <= 0) {
+            if (player.transform.position.y - 0.25f * playerCol.size.y > transform.position.y + 0.25f * myBCol.size.y) {
+                myBCol.enabled = true;
+            }
         }
         else {
             myBCol.enabled = false;
