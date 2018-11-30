5cc2108aa1223b1d9059e07491e1a84d704be770
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-11-30 13:35:32 +0800

    fucking paige goddamnit

--- Anxious Platformer/Assets/Scripts/Hazards/Enemies/Flying Enemy.cs
+++ Anxious Platformer/Assets/Scripts/Hazards/Enemies/Flying Enemy.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class FlyingEnemy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    GameObject target;
+    private void Start() {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
 }
