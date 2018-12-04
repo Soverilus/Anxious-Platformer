a98660e1c280653cc2997195991f4cb97010c0c5
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-12-04 09:21:11 +0800

    update

---
+++ Anxious Platformer/Assets/SceneManagmerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManagmerScript : MonoBehaviour {
+public void QuitApplication() {
+        QuitApplication();
+    }
+
+    public void LoadScene(string myScene) {
+        SceneManager.LoadScene(myScene);
+    }
+}
--- Anxious Platformer/Assets/SpawnAndControlTextRandom.cs
+++ Anxious Platformer/Assets/SpawnAndControlTextRandom.cs
@@ -8,35 +8,51 @@ public class SpawnAndControlTextRandom : MonoBehaviour {
     public string[] myDayTexts;
     public string[] myThoughts;
     public Text myPrefab;
-    float intensity = 1f;
+    public float intensity = 1f;
     float deltaTimeIntense;
     float timer = 0f;
     StatHandler mySH;
+    MovementStats myMS;
     Text myText;
     public Canvas myCanvas;
+    int howManyTimers;
+
+    float timeLeft;
+    float maxTime;
+    float timeMultiplier;
 
     private void Start() {
+        myMS = GameObject.FindGameObjectWithTag("Player").GetComponent<MovementStats>();
         mySH = GameObject.FindGameObjectWithTag("StatHandler").GetComponent<StatHandler>();
+        maxTime = myMS.maxTime;
+        timeLeft = 0;
     }
 
     private void Update() {
-        deltaTimeIntense = Time.deltaTime * intensity;
+        Mathf.Clamp(timeLeft += Time.deltaTime, 0f, maxTime);
+        timeMultiplier = timeLeft / maxTime;
+        deltaTimeIntense = Time.deltaTime * intensity * timeMultiplier;
         timer += deltaTimeIntense;
 
         if (timer >= 3) {
-            myText = Instantiate(myPrefab, gameObject.transform);
-            myText.rectTransform.localPosition = new Vector3(Random.Range(-myCanvas.pixelRect.size.x / 2f, myCanvas.pixelRect.size.x / 2f), Random.Range(-myCanvas.pixelRect.size.y / 2f, myCanvas.pixelRect.size.y / 2f), 0f);
-            myFades.Add(false);
-            myTexts.Add(myText);
-            myText.color = new Color(myText.color.r, myText.color.g, myText.color.b, 0f);
-            myText.text = myThoughts[Random.Range(0, myThoughts.Length)];
-            int r = Random.Range(0, 10);
-            if (r == 0) {
-                myText.text = myDayTexts[mySH.dayNumber];
+            howManyTimers = Mathf.RoundToInt(timer / 3f);
+            for (int k = howManyTimers; k > 0; k--) {
+                myText = Instantiate(myPrefab, gameObject.transform);
+                myText.rectTransform.localPosition = new Vector3(Random.Range(-myCanvas.pixelRect.size.x / 2f, myCanvas.pixelRect.size.x / 2f), Random.Range(-myCanvas.pixelRect.size.y / 2f, myCanvas.pixelRect.size.y / 2f), 0f);
+                myFades.Add(false);
+                myTexts.Add(myText);
+                myText.color = new Color(myText.color.r, myText.color.g, myText.color.b, 0f);
+                myText.text = myThoughts[Random.Range(0, myThoughts.Length)];
+                int r = Random.Range(0, 10);
+                if (r == 0) {
+                    myText.text = myDayTexts[mySH.dayNumber];
+                }
+                intensity += 0.2f;
+                timer = 0f;
             }
-            intensity += 0.2f;
-            timer = 0f;
         }
+
+        //Seperate script for fading in and out
         for (int i = 0; i < myTexts.Count; i++) {
             FadeInOutFunct(myTexts[i], i);
         }
