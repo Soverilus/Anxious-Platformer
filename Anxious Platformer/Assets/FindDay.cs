074d20738b764653d7b016fafa8b8c5177d73999
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-12-14 17:18:03 +0800

    update

---
+++ Anxious Platformer/Assets/FindDay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FindDay : MonoBehaviour {
+    Text thisText;
+    StatHandler mySH;
+    int day;
+    public string[] whichDayIsIt;
+
+    private void Start() {
+        thisText = GetComponent<Text>();
+        mySH = GameObject.FindGameObjectWithTag("StatHandler").GetComponent<StatHandler>();
+        day = mySH.dayNumber;
+
+        thisText.text = whichDayIsIt[day];
+    }
+}
